13c5328ee54568cf381dbcd204a84074d97d175b
marioabbey <5434406+marioabbey@users.noreply.github.com>
2023-05-18 09:29:27 +0100

    still using javascript

--- generate_random_details.js
+++ generate_random_details.js
@@ -4,32 +4,67 @@ const fs = require("fs");
 const numDetails = 100000;
 
 // Generate random details
-const details = [];
+const details = new Array(numDetails);
+
+// Set to store used mobile numbers
 const usedMobileNumbers = new Set();
-const usedCardExpiryDates = new Set();
+
+// Generate random PAN
+let pan = "";
+
+// Helper function to generate a random BIN based on PAN
+const generateRandomBIN = () => {
+  const firstDigits = ["103", "506"];
+  const randomIndex = Math.floor(Math.random() * firstDigits.length);
+  const firstThreeDigits = firstDigits[randomIndex];
+  const remainingDigits = Math.floor(Math.random() * 1000)
+    .toString()
+    .padStart(3, "0");
+  return `${firstThreeDigits}${remainingDigits}`;
+};
+
+// Helper function to generate a random PAN based on BIN
+const generateRandomPAN = (bin) => {
+  const middleDigits = "******";
+  const lastFourDigits = Math.floor(Math.random() * 10000)
+    .toString()
+    .padStart(4, "0");
+  return `${bin}${middleDigits}${lastFourDigits}`;
+};
+
+// Helper function to generate a random date of birth
+const generateRandomDOB = () => {
+  const currentYear = new Date().getFullYear();
+  const minAge = 20;
+  const maxAge = 60;
+  const birthYear = currentYear - getRandomNumber(minAge, maxAge);
+  const birthMonth = getRandomNumber(1, 12);
+  const birthDay = getRandomNumber(1, 28);
+  const date = new Date(birthYear, birthMonth - 1, birthDay);
+  return date;
+};
+
 for (let i = 0; i < numDetails; i++) {
   const recordNumber = i + 1;
 
   // Generate unique mobile number starting with '234'
   let mobileNumber;
   do {
-    mobileNumber = "234" + getRandomNumber(100000000, 999999999);
+    mobileNumber =
+      "234" + getRandomNumber(100000000, 999999999).toString().padStart(9, "0");
   } while (usedMobileNumbers.has(mobileNumber));
   usedMobileNumbers.add(mobileNumber);
 
-  // Generate unique card expiry date in 'MMYY' format
-  let cardExpiryDate;
-  do {
-    const currentDate = new Date();
-    cardExpiryDate =
-      getRandomNumber(1, 12).toString().padStart(2, "0") +
-      currentDate.getFullYear().toString().slice(-2);
-  } while (usedCardExpiryDates.has(cardExpiryDate));
-  usedCardExpiryDates.add(cardExpiryDate);
+  // Generate random BIN
+  const bin = generateRandomBIN();
 
-  const pan = generateRandomPAN();
+  // Generate PAN with correct format and matching BIN
+  const pan = generateRandomPAN(bin);
   const hashedPan = ""; // Empty hashed PAN field
-  const binNumber = generateRandomBIN(pan);
+  // Generate random card expiry date in 'MMYY' format
+  const cardExpiryDate =
+    getRandomNumber(1, 12).toString().padStart(2, "0") +
+    getRandomNumber(24, 50).toString().padStart(2, "0"); // Random month (1-12) and random year (24-50)
   const customerSegment = getRandomElement([
     "Segment A",
     "Segment B",
@@ -51,11 +86,11 @@ for (let i = 0; i < numDetails; i++) {
   const maritalStatus = getRandomElement(["Single", "Married", "Divorced"]);
   const preferredLanguage = getRandomElement(["English", "French", "Spanish"]);
 
-  details.push([
+  details[i] = [
     recordNumber,
     pan,
     hashedPan,
-    binNumber,
+    bin,
     cardExpiryDate,
     customerSegment,
     customerType,
@@ -74,53 +109,9 @@ for (let i = 0; i < numDetails; i++) {
     profession,
     maritalStatus,
     preferredLanguage,
-  ]);
-    // Log progress
-    console.log(`Generated record ${recordNumber}`);
-}
-
-// Helper function to get a random element from an array
-function getRandomElement(array) {
-  return array[Math.floor(Math.random() * array.length)];
-}
-
-// Helper function to generate a random number between min and max (inclusive)
-function getRandomNumber(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-// Helper function to format a date as "YYYY-MM-DD"
-function formatDate(date) {
-  const year = date.getFullYear();
-  const month = String(date.getMonth() + 1).padStart(2, "0");
-  const day = String(date.getDate()).padStart(2, "0");
-  return `${year}-${month}-${day}`;
-}
-
-// Helper function to generate a random PAN
-function generateRandomPAN() {
-  const firstSixDigits = "506970";
-  const lastSevenDigits = getRandomNumber(1000000, 9999999);
-  return firstSixDigits + "*********" + lastSevenDigits;
-}
-
-// Helper function to generate a random BIN based on PAN
-function generateRandomBIN(pan) {
-  const firstSixDigits = pan.substr(0, 6);
-  const validBINPrefixes = ["506", "106"];
-  const validBINPrefix = getRandomElement(validBINPrefixes);
-  return validBINPrefix + firstSixDigits.substr(3, 3);
-}
-
-// Helper function to generate a random date of birth
-function generateRandomDOB() {
-  const currentYear = new Date().getFullYear();
-  const minAge = 20;
-  const maxAge = 60;
-  const birthYear = currentYear - getRandomNumber(minAge, maxAge);
-  const birthMonth = getRandomNumber(1, 12);
-  const birthDay = getRandomNumber(1, 28);
-  return new Date(birthYear, birthMonth - 1, birthDay);
+  ];
+  // Log progress
+  console.log(`Generated record ${recordNumber}`);
 }
 
 // Convert details array to CSV string
@@ -128,3 +119,21 @@ const csvContent = details.map((detail) => detail.join(",")).join("\n");
 
 // Save CSV content to a file
 fs.writeFileSync("random_details.csv", csvContent, "utf8");
+
+// Helper function to get a random element from an array
+function getRandomElement(array) {
+  return array[Math.floor(Math.random() * array.length)];
+}
+
+// Helper function to generate a random number within a range
+function getRandomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// Helper function to format date as 'YYYY-MM-DD'
+function formatDate(date) {
+  const year = date.getFullYear();
+  const month = (date.getMonth() + 1).toString().padStart(2, "0");
+  const day = date.getDate().toString().padStart(2, "0");
+  return `${year}-${month}-${day}`;
+}
